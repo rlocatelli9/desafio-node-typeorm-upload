726bdc0bcd05931378fe13b437107bd887ae3786
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-02-14 19:25:16 -0300

    feat: create and list transactions and balance

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,50 @@
+import { getCustomRepository } from 'typeorm';
+import { response } from 'express';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 
+import TransactionsRepository from '../repositories/TransactionsRepository';
+// import Category from '../models/Category';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    // const categoryRepository = getRepository(Category);
+
+    // const categoryIsExists = await categoryRepository.findOne({
+    //   where: { type: category },
+    // });
+
+    // let categoryReturned: Category;
+    // if (!categoryIsExists) {
+    //   categoryReturned = categoryRepository.create({
+    //     title: category,
+    //   });
+    //   await categoryRepository.save(categoryReturned);
+    // }
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
